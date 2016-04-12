b7cbcf2dcb932f274526ee6b4cac1a5dcad9192b
FezVrasta <federico.zivolo@gmail.com>
2016-04-12 19:46:19 +0200

    doc

---
+++ api.js
@@ -0,0 +1,171 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Ember",
+        "Ember.ActionHandler",
+        "Ember.Application",
+        "Ember.Array",
+        "Ember.ArrayController",
+        "Ember.ArrayProxy",
+        "Ember.AutoLocation",
+        "Ember.Binding",
+        "Ember.Checkbox",
+        "Ember.CollectionView",
+        "Ember.Comparable",
+        "Ember.Component",
+        "Ember.ComponentTemplateDeprecation",
+        "Ember.ComputedProperty",
+        "Ember.ContainerDebugAdapter",
+        "Ember.ContainerView",
+        "Ember.Controller",
+        "Ember.ControllerMixin",
+        "Ember.Copyable",
+        "Ember.CoreObject",
+        "Ember.CoreView",
+        "Ember.DataAdapter",
+        "Ember.DefaultResolver",
+        "Ember.Deferred",
+        "Ember.Descriptor",
+        "Ember.EachProxy",
+        "Ember.Enumerable",
+        "Ember.EnumerableUtils",
+        "Ember.Error",
+        "Ember.EventDispatcher",
+        "Ember.Evented",
+        "Ember.FEATURES",
+        "Ember.Freezable",
+        "Ember.Handlebars",
+        "Ember.Handlebars.Compiler",
+        "Ember.Handlebars.JavaScriptCompiler",
+        "Ember.Handlebars.helpers",
+        "Ember.HashLocation",
+        "Ember.HistoryLocation",
+        "Ember.Instrumentation",
+        "Ember.LinkView",
+        "Ember.Location",
+        "Ember.Logger",
+        "Ember.Map",
+        "Ember.MapWithDefault",
+        "Ember.Mixin",
+        "Ember.MutableArray",
+        "Ember.MutableEnumerable",
+        "Ember.Namespace",
+        "Ember.NativeArray",
+        "Ember.NoneLocation",
+        "Ember.Object",
+        "Ember.ObjectController",
+        "Ember.ObjectProxy",
+        "Ember.Observable",
+        "Ember.OrderedSet",
+        "Ember.PromiseProxyMixin",
+        "Ember.RSVP",
+        "Ember.RSVP.EventTarget",
+        "Ember.RSVP.Promise",
+        "Ember.ReduceComputedProperty",
+        "Ember.RenderBuffer",
+        "Ember.Route",
+        "Ember.Router",
+        "Ember.Select",
+        "Ember.Set",
+        "Ember.SortableMixin",
+        "Ember.State",
+        "Ember.StateManager",
+        "Ember.String",
+        "Ember.SubArray",
+        "Ember.TargetActionSupport",
+        "Ember.Test",
+        "Ember.Test.Adapter",
+        "Ember.Test.QUnitAdapter",
+        "Ember.TextArea",
+        "Ember.TextField",
+        "Ember.TextSupport",
+        "Ember.TrackedArray",
+        "Ember.View",
+        "Ember.ViewTargetActionSupport",
+        "Ember._HandlebarsBoundView",
+        "Ember._Metamorph",
+        "Ember._MetamorphView",
+        "Ember._SimpleMetamorphView",
+        "Ember.platform",
+        "Function",
+        "String",
+        "run.run"
+    ],
+    "modules": [
+        "RSVP",
+        "ember",
+        "ember-application",
+        "ember-debug",
+        "ember-extension-support",
+        "ember-handlebars",
+        "ember-handlebars-compiler",
+        "ember-metal",
+        "ember-routing",
+        "ember-runtime",
+        "ember-testing",
+        "ember-views"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember",
+            "name": "ember",
+            "description": "Ember"
+        },
+        {
+            "displayName": "ember-application",
+            "name": "ember-application",
+            "description": "Ember Application"
+        },
+        {
+            "displayName": "ember-debug",
+            "name": "ember-debug",
+            "description": "Ember Debug"
+        },
+        {
+            "displayName": "ember-extension-support",
+            "name": "ember-extension-support",
+            "description": "Ember Extension Support"
+        },
+        {
+            "displayName": "ember-handlebars",
+            "name": "ember-handlebars",
+            "description": "Ember Handlebars"
+        },
+        {
+            "displayName": "ember-handlebars-compiler",
+            "name": "ember-handlebars-compiler",
+            "description": "Prepares the Handlebars templating library for use inside Ember's view\nsystem.\n\nThe `Ember.Handlebars` object is the standard Handlebars library, extended to\nuse Ember's `get()` method instead of direct property access, which allows\ncomputed properties to be used inside templates.\n\nTo create an `Ember.Handlebars` template, call `Ember.Handlebars.compile()`.\nThis will return a function that can be used by `Ember.View` for rendering."
+        },
+        {
+            "displayName": "ember-metal",
+            "name": "ember-metal",
+            "description": "All Ember methods and functions are defined inside of this namespace. You\ngenerally should not add new properties to this namespace as it may be\noverwritten by future versions of Ember.\n\nYou can also use the shorthand `Em` instead of `Ember`.\n\nEmber-Runtime is a framework that provides core functions for Ember including\ncross-platform functions, support for property observing and objects. Its\nfocus is on small size and performance. You can use this in place of or\nalong-side other cross-platform libraries such as jQuery.\n\nThe core Runtime framework is based on the jQuery API with a number of\nperformance optimizations."
+        },
+        {
+            "displayName": "ember-routing",
+            "name": "ember-routing",
+            "description": "Ember Routing"
+        },
+        {
+            "displayName": "ember-runtime",
+            "name": "ember-runtime",
+            "description": "Ember Runtime"
+        },
+        {
+            "displayName": "ember-testing",
+            "name": "ember-testing",
+            "description": "Ember Testing"
+        },
+        {
+            "displayName": "ember-views",
+            "name": "ember-views",
+            "description": "Ember Views"
+        },
+        {
+            "displayName": "RSVP",
+            "name": "RSVP"
+        }
+    ],
+    "elements": []
+} };
+});
